48b45ecea5ffbe3e70542332aab0a628fb71413d
Daniel Lee <danlee746@hotmail.ca>
2017-06-01 15:08:15 -0400

    Matrix operations using C#

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Matrix
+    {
+        private int theRows;
+        private int theCols;
+        private int[,] aMatrix;
+        Random random = new Random();
+
+        public Matrix() {}
+
+        public Matrix(int rows, int cols)
+        {
+            NewMatrix = new int[rows,cols];
+            Rows = rows;
+            Cols = cols;
+            
+        }
+
+        // Destructor
+        ~Matrix() 
+        {
+            GC.Collect();
+            Console.WriteLine("Finished!");
+        }
+
+        public int Sum { get; set; }
+        //Default properties possible here
+        public int Rows 
+        { 
+            get
+            {
+                return theRows;
+            }
+            set
+            {
+                theRows = value;
+            }
+        
+        }
+
+        public int Cols
+        {
+            get
+            {
+                return theCols;
+            }
+            set
+            {
+                theCols = value;
+            }
+
+        }
+
+        public int[,] NewMatrix
+        {
+            get {
+                return aMatrix; 
+            }
+            set {
+                aMatrix = value;
+            }
+        }
+        //Method that Fill our Matrix with random integers
+        public void FillMatrix()
+        {   
+            for(int i = 0; i < this.Rows; i++)
+            {
+                for(int j = 0; j < this.Cols; j++)
+                {
+                    this.NewMatrix[i, j] = this.random.Next(0,10);
+                }
+            }
+        }
+        //Displays our lovely Matrix to console
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < this.NewMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.NewMatrix.GetLength(1); j++)
+                {
+                    Console.Write(this.NewMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        //Adding two matrices
+        public static Matrix operator +(Matrix m1, Matrix m2)
+        { 
+            //m1 since both matrices have the same rows and cols anyways
+            Matrix temp = new Matrix(m1.Rows, m1.Cols);
+            int result;
+            for(int i = 0; i < m1.NewMatrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < m1.NewMatrix.GetLength(1); j++)
+                {
+                    result = m1.NewMatrix[i, j] + m2.NewMatrix[i, j];
+                    temp.NewMatrix[i,j] = result;
+                }
+            }
+            return temp;
+        }
+
+        
+        //Multiplying two matrices
+        
+        public static Matrix operator *(Matrix m1, Matrix m2)
+        {
+            Matrix temp = new Matrix(m1.Rows, m2.Cols);
+            
+            for (int row = 0; row < m1.NewMatrix.GetLength(0); row++)
+            {
+                for(int col = 0; col < m2.NewMatrix.GetLength(1); col++)
+                {
+                    int sum = 0;
+                    for(int acols = 0; acols < m1.NewMatrix.GetLength(1); acols++)
+                    {
+                        sum += m1.NewMatrix[row, acols] * m2.NewMatrix[acols, col]; 
+                    }
+                    temp.NewMatrix[row, col] = sum;
+                }
+            } 
+            return temp;
+        }
+         
+        //Transpose Matrix
+        public static Matrix operator !(Matrix m)
+        {
+            Matrix temp = new Matrix(m.Cols,m.Rows);
+            for (int i = 0; i < m.NewMatrix.GetLength(0);i++)
+            {
+                for (int j = 0; j < m.NewMatrix.GetLength(1);j++)
+                {
+                    temp.NewMatrix[j,i] = m.NewMatrix[i,j];
+                }
+            }
+            return temp;
+        }
+        //Trace of a Matrix
+        public static Matrix operator --(Matrix m)
+        {
+            Matrix temp = new Matrix(m.Rows,m.Cols);
+            
+            int sum = 0;
+            int rowcount = 0;
+            
+            for (int i = 0; i  < m.NewMatrix.GetLength(0);i++)
+            {
+                for(int j = 0; j < m.NewMatrix.GetLength(1);j++)
+                {
+                    temp.NewMatrix[i, j] = m.NewMatrix[i, j];
+                    if (j == rowcount)
+                    {
+                        sum += m.NewMatrix[i, j];
+                        
+                    }
+                }
+                rowcount++;
+            }
+
+            temp.Sum = sum;
+            return temp;
+        }
+    }
+}
---
+++ Matrix/Tester.cs
@@ -0,0 +1,144 @@
+﻿//Matrix operator overloading
+//Author: Daniel Lee
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Tester
+    {
+        private static Matrix M1;
+        private static Matrix M2;
+        private static Matrix M3;
+
+        static void Main(string[] args)
+        {
+            createMatrices();
+
+            while(true)
+            {
+                Console.WriteLine(@"
+Enter 0 to exit the program
+Enter 1 if you want to add the matrices
+Enter 2 if you want to multiply the matrices
+Enter 3 if you want to transpose the first matrix
+Enter 4 if you want to get the trace of the first matrix
+Enter 5 if you want to display the two matrices
+Enter 6 to generate new matrices:");
+                int choice = 0;
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+                } catch(FormatException fe)
+                {
+                    Console.WriteLine("Error: " + fe);
+                    continue;
+                }
+                switch(choice)
+                {
+                    case 0:
+                        Console.WriteLine("Exiting...");
+                        System.Environment.Exit(1);
+                        break;
+                    case 1:
+                        Console.WriteLine("Added Matrix:");
+                        if ((M1.Rows != M2.Rows) && (M1.Cols != M2.Cols))
+                        {
+                            Console.WriteLine("Rows and columns of first and second matrix must be equal");
+                            break;
+                        }
+                        M3 = M1 + M2;
+                        M3.PrintMatrix();
+                        
+                        break;
+                    case 2:
+                        
+                        Console.WriteLine("Multiplied Matrix:");
+                        if(M1.Cols != M2.Rows)
+                        {
+                            Console.WriteLine("Number of columns in first Matrix must be equal to rows in the 2nd");
+                            break;
+                        }
+                        M3 = M1 * M2;
+                        M3.PrintMatrix();
+                       
+                        break;
+                    case 3:
+                        Console.WriteLine("Transposed Matrix:");
+                        M3 = !M1;
+                        M3.PrintMatrix();
+                        break;
+                    case 4:
+                        Console.WriteLine("Trace of a Matrix:");
+                        if(M1.Rows != M1.Cols)
+                        {
+                            Console.WriteLine("It must be a n by n matrix");
+                            break;
+                        }
+                        M3 = --M1;
+                        Console.WriteLine(M3.Sum);
+                        break;
+                    case 5:
+                        Console.WriteLine("Matrix 1: ");
+                        M1.PrintMatrix();
+                        Console.WriteLine("Matrix 2: ");
+                        M2.PrintMatrix();
+                        break;
+                    case 6:
+                        createMatrices();
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
+
+        public static void createMatrices()
+        {
+            int row1 = 0;
+            int col1 = 0;
+            int row2 = 0;
+            int col2 = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter number of rows for matrix 1: ");
+                    row1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter number of cols for matrix 1: ");
+                    col1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter number of rows for matrix 2: ");
+                    row2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter number of cols for matrix 2: ");
+                    col2 = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine("Error: " + fe);
+                    continue;
+                }
+            }
+
+            //I created two Matrix instances
+            M1 = new Matrix(row1, col1);
+            //I filled the matrix with random values up to 10 using my method
+            M1.FillMatrix();
+
+            M2 = new Matrix(row2, col2);
+            M2.FillMatrix();
+
+            M3 = new Matrix();
+
+            Console.WriteLine("Matrix 1: ");
+            M1.PrintMatrix();
+            Console.WriteLine("Matrix 2: ");
+            M2.PrintMatrix();
+        }
+    }
+}
